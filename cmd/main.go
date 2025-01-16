2e16a471912522f57a04ebcde4f3141e6fc451bc
umed-hotamov <uhotamov314@gmail.com>
2025-01-16 17:00:28 +0500

    init

--- cmd/main.go
+++ cmd/main.go
@@ -1,4 +1,7 @@
 package main
 
+import "github.com/umed-hotamov/house-rental/internal/app"
+
 func main() {
+  app.Run()
 }
--- internal/app/app.go
+++ internal/app/app.go
@@ -1,5 +1,23 @@
 package app
 
-func Run() {
+import (
+	"net/http"
 
+	"github.com/umed-hotamov/house-rental/internal/app/config"
+	"github.com/umed-hotamov/house-rental/internal/app/server"
+
+	"go.uber.org/fx"
+)
+
+func Run() {
+  cfg := config.GetConfig()
+
+  fx.New(
+    fx.Provide(
+      server.NewEchoRouter,
+      server.NewServer,
+    ),
+    fx.Supply(cfg),
+    fx.Invoke(func(*http.Server) {}),
+  ).Run()
 }
