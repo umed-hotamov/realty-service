ef007e1f8b4a731b01b05f5c1216bb749b4f940f
umed-hotamov <umed.hotamov5858@gmail.com>
2025-01-24 17:04:09 +0500

    postgres added

---
+++ pkg/database/postgres/postgres.go
@@ -0,0 +1,41 @@
+package postgres
+
+import (
+	"fmt"
+	"time"
+
+	"github.com/umed-hotamov/realty-service/internal/app/config"
+
+	"go.uber.org/zap"
+	_ "github.com/jackc/pgx/v5/stdlib"
+	"github.com/jmoiron/sqlx"
+)
+
+const (
+  maxConns            = 50
+  maxIdleConns        = 25
+  maxConnLifeTime     = 10 * time.Minute
+  maxIdleConnTime     = 5 * time.Minute
+)
+
+func NewPostgresDB(cfg *config.Config, logger *zap.Logger) *sqlx.DB {
+  dsn := fmt.Sprintf("host=%s port%s dbname=%s password=%s",
+                      cfg.Postgres.Host,
+                      cfg.Postgres.Port,
+                      cfg.Postgres.DBName,
+                      cfg.Postgres.Password)
+
+  db, err := sqlx.Connect("pgx", dsn) 
+
+  if err != nil {
+    logger.Fatal("failed to connect to postgres: %s", zap.String("dsn", dsn))
+    return nil
+  }
+
+  db.SetMaxOpenConns(maxConns)
+  db.SetMaxIdleConns(maxIdleConns)
+  db.SetConnMaxLifetime(maxConnLifeTime)
+  db.SetConnMaxIdleTime(maxIdleConnTime)
+
+  return db
+}
