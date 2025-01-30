561da1003455b028ef410af198371a900e0f6817
umed-hotamov <umed.hotamov5858@gmail.com>
2025-01-30 17:31:50 +0500

    init

---
+++ internal/domain/id.go
@@ -0,0 +1,15 @@
+package domain
+
+import (
+  "github.com/google/uuid"
+)
+
+type ID string
+
+func (id ID) String() string {
+  return string(id)
+}
+
+func NewID() ID {
+  return ID(uuid.New().String())
+}
