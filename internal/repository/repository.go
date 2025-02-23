694fc977bd2f5aad711341fe58a6b5c462f100ef
umed-hotamov <umed.hotamov5858@gmail.com>
2025-02-23 12:58:23 +0500

    new interface

--- internal/repository/repository.go
+++ internal/repository/repository.go
@@ -8,18 +8,18 @@ import (
 
 type IUserRepo interface {
   GetAll(ctx context.Context) ([]domain.User, error)
-  GetByID(ctx context.Context) (domain.User, error)
+  GetByID(ctx context.Context, id domain.ID) (domain.User, error)
+  GetByRole(ctx context.Context, role domain.UserRole) (domain.User, error)
   Create(ctx context.Context, user domain.User) (domain.User, error)
   Update(ctx context.Context, user domain.User) (domain.User, error)
-  Delete(ctx context.Context, userID int) error
+  Delete(ctx context.Context, userID domain.ID) error
 }
 
 type IPropertyRepo interface {
   GetAll(ctx context.Context) ([]domain.Property, error)
-  GetPropertyByAddress(ctx context.Context, address string) ([]domain.Property, error)
-  GetUserProperty(ctx context.Context, userID int) ([]domain.Property, error)
-  GetApprovedProperty(ctx context.Context) ([]domain.Property, error)
-  GetFilteredProperty(ctx context.Context, filter domain.PropertyFilter) ([]domain.Property, error)
+  GetPropertiesByAddress(ctx context.Context, address string) ([]domain.Property, error)
+  GetUserProperties(ctx context.Context, userID domain.ID) ([]domain.Property, error)
+  GetPropertyDetails(ctx context.Context, propertyID domain.ID) (domain.Property, error)
   Create(ctx context.Context, property domain.Property) (domain.Property, error)
   Update(ctx context.Context, property domain.Property) (domain.Property, error)
   Delete(ctx context.Context, property domain.Property) error
@@ -31,10 +31,4 @@ type IListingRepo interface {
   Create(ctx context.Context, listing domain.Listing) (domain.Listing, error)
   Update(ctx context.Context, listing domain.Listing) (domain.Listing, error)
   Delete(ctx context.Context, listingID int) error
-}
-
-type IApartmentBuildingRepo interface {
-  GetAll(ctx context.Context) ([]domain.ApartmentBuilding, error)
-  GatBuildingFlats(ctx context.Context, buildingID int) ([]domain.Flat, error)
-  Create(ctx context.Context, building domain.ApartmentBuilding) (domain.ApartmentBuilding, error)
 }
