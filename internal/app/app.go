cbbd53303f560cf5f7313e83ea1d8ddb1ca40370
umed-hotamov <umed.hotamov5858@gmail.com>
2025-03-04 12:42:55 +0500

    upd

--- internal/app/app.go
+++ internal/app/app.go
@@ -3,21 +3,44 @@ package app
 import (
 	"net/http"
 
+	v1 "github.com/umed-hotamov/realty-service/internal/handler/api/v1"
 	"github.com/umed-hotamov/realty-service/internal/app/config"
 	"github.com/umed-hotamov/realty-service/internal/app/server"
+	"github.com/umed-hotamov/realty-service/internal/repository"
+	pg "github.com/umed-hotamov/realty-service/internal/repository/postgres"
+	"github.com/umed-hotamov/realty-service/internal/service"
+	"github.com/umed-hotamov/realty-service/internal/service/providers"
+	database "github.com/umed-hotamov/realty-service/pkg/database/postgres"
+	"github.com/umed-hotamov/realty-service/pkg/logger"
 
 	"go.uber.org/fx"
 )
 
 func Run() {
   cfg := config.GetConfig()
+  logger := logger.NewLogger(cfg)
+  
+  logger.Info("start app")
 
   fx.New(
     fx.Provide(
       server.NewEchoRouter,
       server.NewServer,
+      v1.NewHandler,
+      
+      database.NewPostgresDB,
+    
+      fx.Annotate(
+        pg.NewUserRepo,
+        fx.As(new(repository.IUserRepo)),        
+      ),
+
+      fx.Annotate(
+        providers.NewUserService,
+        fx.As(new(service.IUserService)),
+      ),
     ),
-    fx.Supply(cfg),
+    fx.Supply(cfg, logger),
     fx.Invoke(func(*http.Server) {}),
   ).Run()
 }
--- internal/app/server/server.go
+++ internal/app/server/server.go
@@ -3,23 +3,22 @@ package server
 import (
 	"context"
 	"fmt"
-	"log"
 	"net/http"
 	"time"
 
+	v1 "github.com/umed-hotamov/realty-service/internal/handler/api/v1"
 	"github.com/umed-hotamov/realty-service/internal/app/config"
 
-	"github.com/labstack/echo/v4/middleware"
 	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
 	"go.uber.org/fx"
+	"go.uber.org/zap"
 )
 
 func NewEchoRouter() *echo.Echo {
   e := echo.New()
-  
   e.Use(middleware.Logger())
   e.GET("/ping", func(c echo.Context) error {
-      time.Sleep(15 * time.Second)
       return c.String(http.StatusOK, "Pong")
   })
 
@@ -28,9 +27,11 @@ func NewEchoRouter() *echo.Echo {
 
 type ServerParams struct {
   fx.In
-
-  Router *echo.Echo
-  Cfg    *config.Config
+  
+  Router  *echo.Echo
+  Cfg     *config.Config
+  Logger  *zap.Logger
+  Handler *v1.Handler
 }
 
 func NewServer(lc fx.Lifecycle, params ServerParams) *http.Server {
@@ -40,12 +41,11 @@ func NewServer(lc fx.Lifecycle, params ServerParams) *http.Server {
     ReadTimeout:  15 * time.Second,
     WriteTimeout: 15 * time.Second,
   }
-  
   lc.Append(fx.Hook{
     OnStart: func(ctx context.Context) error {
       go func() {
-        log.Printf("server started on port: %s\n", params.Cfg.Server.Port)
-        server.ListenAndServe()
+        params.Logger.Info(fmt.Sprintf("server started on port: %s\n", params.Cfg.Server.Port))
+        params.Logger.Fatal("stop server", zap.Error(server.ListenAndServe()))
       }()
 
       return nil
