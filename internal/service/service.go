fbb5b562782addad79f9937d23beac5a0116b57c
umed-hotamov <umed.hotamov5858@gmail.com>
2025-03-04 12:44:51 +0500

    upd

--- internal/service/service.go
+++ internal/service/service.go
@@ -6,14 +6,29 @@ import (
 	"github.com/umed-hotamov/realty-service/internal/domain"
 )
 
-type IHouseService interface {
-  GetAll(ctx context.Context) ([]domain.House, error)
-  Create(ctx context.Context, house domain.House) (domain.House, error)
+type IUserService interface {
+  GetAll(ctx context.Context) ([]domain.User, error)
+  GetByID(ctx context.Context, id domain.ID) (domain.User, error)
+  GetByRole(ctx context.Context, role domain.UserRole) ([]domain.User, error)
+  Create(ctx context.Context, param domain.CreateUserParam) (domain.User, error)
+  Update(ctx context.Context, param domain.UpdateUserParam, userID domain.ID) (domain.User, error)
+  Delete(ctx context.Context, userID domain.ID) error
 }
 
-type IFlatService interface {
-  GetFlatsByHouseID(ctx context.Context, houseID int) ([]domain.Flat)
-  GetApprovedFlatsByHouseID(ctx context.Context, houseID int) ([]domain.Flat, error)
-  Create(ctx context.Context, flat domain.Flat) (domain.Flat, error)
-  Update(ctx context.Context, flatID int) (domain.Flat, error)
+type IPropertyService interface {
+  GetAll(ctx context.Context) ([]domain.Property, error)
+  GetPropertiesByAddress(ctx context.Context, address string) ([]domain.Property, error)
+  GetUserProperties(ctx context.Context, userID domain.ID) ([]domain.Property, error)
+  GetPropertyDetails(ctx context.Context, propertyID domain.ID) (domain.Property, error)
+  Create(ctx context.Context, property domain.Property) (domain.Property, error)
+  Update(ctx context.Context, property domain.Property) (domain.Property, error)
+  Delete(ctx context.Context, property domain.Property) error
+}
+
+type IListingService interface {
+  GetAll(ctx context.Context) ([]domain.Listing, error)
+  GetUserListings(ctx context.Context, userID int) ([]domain.Listing, error)
+  Create(ctx context.Context, listing domain.Listing) (domain.Listing, error)
+  Update(ctx context.Context, listing domain.Listing) (domain.Listing, error)
+  Delete(ctx context.Context, listingID int) error
 }
