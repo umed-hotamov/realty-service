604e9c86f98db8fb29d9aecaa72ae6a1f5fe5047
umed-hotamov <umed.hotamov5858@gmail.com>
2025-03-25 16:46:11 +0500

    upd

--- internal/errs/errors.go
+++ internal/errs/errors.go
@@ -3,9 +3,17 @@ package errs
 import "errors"
 
 var (
-	ErrDuplicate         = errors.New("record already exists")
-	ErrNotExist          = errors.New("record does not exist")
-	ErrUpdateFailed      = errors.New("record update failed")
-	ErrDeleteFailed      = errors.New("record delete failed")
-	ErrPersistenceFailed = errors.New("persistence internal error")
+	ErrDuplicate          = errors.New("record already exists")
+	ErrNotExist           = errors.New("record does not exist")
+	ErrUpdateFailed       = errors.New("record update failed")
+	ErrDeleteFailed       = errors.New("record delete failed")
+	ErrPersistenceFailed  = errors.New("persistence internal error")
+)
+
+var (
+  ErrInvalidTokenClaims = errors.New("token or claims are invalid")
+  ErrInvalidFingerprint = errors.New("invalid client fingerprint")
+  ErrInvalidToken       = errors.New("invalid jwt token")
+	ErrEmail              = errors.New("email error")
+	ErrPassword           = errors.New("password error")
 )
